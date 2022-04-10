248ad3a943d3e18c852b2e7071c214040d1d5fec
Davidmccoy4 <73482046+Davidmccoy4@users.noreply.github.com>
2022-04-10 19:27:44 -0400

    map function

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -31,6 +31,19 @@ function createHtmlList(collection) {
   });
 }
 
+function initMap(targetId) {
+  const map = L.map('map').setView([51.505, -0.09], 13);
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
+  }).addTo(map);
+  return map;
+}
+
 async function mainEvent() { // the async keyword means we can make API requests
   console.log('script loaded');
   const form = document.querySelector('.main_form');
@@ -38,11 +51,13 @@ async function mainEvent() { // the async keyword means we can make API requests
 
   const resto = document.querySelector('#resto_name');
   const city = document.querySelector('#city');
+  const map = initMap('map');
   submit.style.display = 'none';
 
-  const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
-  const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+  // const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
+  // const arrayFromJson = await results.json(); // This changes it into data we can use - an object
   // console.log(arrayFromJson);
+  let arrayFromJson = {data: []}; //ToDo
 
   // This if sttement is to prevent a race condition on data load
   if (arrayFromJson.data.length > 0) {
